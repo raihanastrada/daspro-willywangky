c9c64b79ab0ecfd3a16733f09cf151836cf0c2e9
sidarthaprastya <sidarthatjoa@gmail.com>
2020-04-05 18:42:26 +0700

    Perbaikan login

--- login.py
+++ login.py
@@ -1,21 +1,17 @@
 import csv
-csvfile = open('user.csv', newline='')
-filelogin= csv.reader(csvfile)
 
-data = [row for row in filelogin]
+with open('user.csv','r') as read:
+    csv_reader = csv.DictReader(read)
+    un = input("Masukkan username: ")
+    pw = input("Masukkan password: ")
 
-col_nama = [x[0] for x in data]
-col_uname = [x[3] for x in data]
-col_pw = [x[4] for x in data]
-
-un = input("Masukkan username: ")
-pw = input("Masukkan password: ")
-
-login = False
-if un in col_uname:
-    for i in range(0, len(col_uname)):   
-        if col_uname[i]== un and col_pw[i] == pw:
+    login = False
+    for row in csv_reader:
+        if (row['Username']==un and row['Password']==pw):
+            user = row['Nama']
             login = True
-            print("Selamat bermain, {}!").format(col_nama[i])
-else:
-    print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
+            break
+    if (login == True):
+        print("Selamat bermain, {}!".format(user))
+    else:
+        print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
