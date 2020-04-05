b976ff62e29810551d4da24da459226012b3c529
tugusav <61377786+tugusav@users.noreply.github.com>
2020-04-05 22:42:00 +0700

    Add files via upload
    cekk dulu gan

---
+++ cari_pemain.py
@@ -0,0 +1,22 @@
+import csv
+
+with open('user.csv', 'r') as read:
+    csv_reader = csv.DictReader(read)
+    un_search = input("Masukkan username: ")
+    
+    found = False
+    for row in csv_reader:
+         if(row['Username'] == un_search):
+             found = True
+             data = row
+             break
+    if (found == True):
+        nama = row["Nama"]
+        tgl = row["Tanggal_Lahir"]
+        tinggi = row["Tinggi_Badan"]
+
+        print("Nama pemain: " + nama)
+        print("Tinggi badan pemain: " + str(tinggi))
+        print("Tanggal lahir pemain: " + str(tgl))
+    else:
+        print("Maaf, pemain tidak ditemukan di database kami!")
