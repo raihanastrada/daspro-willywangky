62d9c592d42056c6c9733b2b20558995da6458d5
tugusav <61377786+tugusav@users.noreply.github.com>
2020-04-05 22:43:19 +0700

    First attempt cari_pemain

--- cari_pemain.py
+++ cari_pemain.py
@@ -1,22 +1,22 @@
 import csv
+def cari_pemain():
+    with open('user.csv', 'r') as read:
+        csv_reader = csv.DictReader(read)
+        un_search = input("Masukkan username: ")
 
-with open('user.csv', 'r') as read:
-    csv_reader = csv.DictReader(read)
-    un_search = input("Masukkan username: ")
-    
-    found = False
-    for row in csv_reader:
-         if(row['Username'] == un_search):
-             found = True
-             data = row
-             break
-    if (found == True):
-        nama = row["Nama"]
-        tgl = row["Tanggal_Lahir"]
-        tinggi = row["Tinggi_Badan"]
+        found = False
+        for row in csv_reader:
+             if(row['Username'] == un_search):
+                 found = True
+                 data = row
+                 break
+        if (found == True):
+            nama = row["Nama"]
+            tgl = row["Tanggal_Lahir"]
+            tinggi = row["Tinggi_Badan"]
 
-        print("Nama pemain: " + nama)
-        print("Tinggi badan pemain: " + str(tinggi))
-        print("Tanggal lahir pemain: " + str(tgl))
-    else:
-        print("Maaf, pemain tidak ditemukan di database kami!")
+            print("Nama pemain: " + nama)
+            print("Tinggi badan pemain: " + str(tinggi))
+            print("Tanggal lahir pemain: " + str(tgl))
+        else:
+            print("Maaf, pemain tidak ditemukan di database kami!")
