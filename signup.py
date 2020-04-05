d82aa2639c0f920b970b9c6577b7765dbc2224b7
sidarthaprastya <sidarthatjoa@gmail.com>
2020-04-05 18:24:01 +0700

    Perbaikan append

--- signup.py
+++ signup.py
@@ -3,20 +3,30 @@ import csv
 with open('user.csv', 'r') as csvread:
     csv_reader = csv.DictReader(csvread)
 
-    with open('user.csv', 'w', newline='') as csvwrite:
+    with open('user.csv', 'a', newline='') as csvwrite:
         fieldnames = ['Nama','Tanggal_Lahir','Tinggi_Badan','Username','Password']
         filesignup = csv.DictWriter(csvwrite, fieldnames=fieldnames)
-        filesignup.writeheader()
         Nama = input("Masukkan nama pemain: ")
         Tanggal_Lahir = input("Masukkan tanggal lahir pemain (DD/MM/YYY): ")
         Tinggi_Badan = input("Masukkan tinggi badan (cm): ")
         Username = input("Masukkan username pemain: ")
         Password = input("Masukkan password pemain: ")
-        if Username in csv_reader:
-            print("Username sudah terdaftar")
-        else:
-            filesignup.writerow({'Nama' : Nama , 'Tanggal_Lahir' : Tanggal_Lahir ,
-            'Tinggi_Badan' : Tinggi_Badan , 'Username' : Username , 'Password' : Password})
+
+        valid = True
+        for row in csv_reader:
+            val_un = row['Username']
+            if (val_un == Username):
+                print("Username sudah terdaftar")
+                valid = False
+                break
+        if (valid == True):
+            filesignup.writerow({
+                'Nama' : Nama ,
+                'Tanggal_Lahir' : Tanggal_Lahir ,
+                'Tinggi_Badan' : Tinggi_Badan , 
+                'Username' : Username , 
+                'Password' : Password
+                })
 
 
     
