60b7cd3b315efc5f6c500cb99b261cb5fae57ed2
Azbleus <63193525+Azbleus@users.noreply.github.com>
2020-04-10 19:26:33 +0700

    load.py
    Added certain file not found error

--- load.py
+++ load.py
@@ -1,54 +1,32 @@
+import csv
+
 def load():
-    fileuser =[]
-    filewahana =[]
-    filepembelian =[]
-    filepenggunaan=[]
-    filekepemilikan =[]
-    filerefund =[]
-    kritiksaran =[]
-    
-    file_user = input("Masukkan nama File User: ")
-    file_wahana = input("Masukkan nama File Wahana:")
-    file_pembelian = input("Masukkan nama File Pembelian Tiket: ")
-    file_penggunaan = input("Masukkan nama File Penggunaan Tiket: ")
-    file_kepemilikan = input("Masukkan nama File Kepemilikan Tiket: ")
-    file_refund = input("Masukkan nama File Refund Tiket: ")
-    file_kritiksaran = input("Masukkan nama File Kritik dan Saran: ")
-    with open(file_user , 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter =',')
-        for row in read:
-            fileuser.append(row)
-    with open(file_wahana, 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter=',')
-        for row in read:
-            filewahana.append(row)
-    with open(file_pembelian, 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter=',')
-        for row in read:
-            filewahana.append(row)
-    with open(file_pembelian, 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter=',')
-        for row in read:
-            filepembelian.append(row)
-    with open(file_penggunaan, 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter=',')
-        for row in read:
-            filepenggunaan.append(row)
-    with open(file_kepemilikan, 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter=',')
-        for row in read:
-            filekepemilikan.append(row)
-    with open(file_refund, 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter=',')
-        for row in read:
-            filerefund.append(row)
-    with open(file_kritiksaran, 'r') as csv_file:
-        read = csv.DictReader(csv_file, delimiter=',')
-        for row in read:
-            kritiksaran.append(row)
-    print("File perusahaan Willy Wangky’s Chocolate Factory telah di-load.")
-    
+    fileuser = []
+    filewahana = []
+    filepembelian = []
+    filepenggunaan = []
+    filekepemilikan = []
+    filerefund = []
+    filekritiksaran = []
+    Filename = [fileuser,filewahana,filepembelian,filepembelian,filepenggunaan,filekepemilikan,filerefund,filekritiksaran]
+    File = ['' for i in range(7)]
+    File[0] = input('Masukkan nama File User: ')
+    File[1] = input('Masukkan nama File Daftar Wahana: ')
+    File[2] = input('Masukkan nama File Pembelian Tiket: ')
+    File[3] = input('Masukkan nama File Penggunaan Tiket: ')
+    File[4] = input('Masukkan nama File Kepemilikan Tiket: ')
+    File[5] = input('Masukkan nama File Refund Tiket: ')
+    File[6] = input('Masukkan nama File Kritik dan Saran: ')
+    Error = [False for i in range(7)]
+    for i in range(7):
+        try:
+            with open(File[i], 'r') as csv_file:
+                read = csv.DictReader(csv_file, delimiter=',')
+                for row in read:
+                    Filename[i].append(row)
+        except:
+            print('Error! File',File[i],'not found!')
+            Error[i] = True
+    if (Error.count(True) == 0):
+        print('File perusahaan Willy Wangky’s Chocolate Factory telah di-load.')
     return fileuser, filewahana, filepembelian, filepenggunaan, filekepemilikan, filerefund, kritiksaran
-
-
-
