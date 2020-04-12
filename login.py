823c2d61ef3deefe9ea702cb37ed674a5abc6bde
tugusav <61377786+tugusav@users.noreply.github.com>
2020-04-13 00:08:28 +0700

    update login.py

--- login.py
+++ login.py
@@ -1,18 +1,48 @@
 import csv
 import Obfuscator
 
-with open('user.csv','r') as read:
-    csv_reader = csv.DictReader(read)
-    un = input("Masukkan username: ")
-    pw = Obfuscator.obs(input("Masukkan password: "))
+import csv
+from load import Filename
 
+'''
+File[0] = 'user.csv'        # input('Masukkan nama File User: ')
+File[1] = 'wahana.csv'      # input('Masukkan nama File Daftar Wahana: ')
+File[2] = 'pembelian.csv'   # input('Masukkan nama File Pembelian Tiket: ')
+File[3] = 'penggunaan.csv'  # input('Masukkan nama File Penggunaan Tiket: ')
+File[4] = 'tiket.csv'       # input('Masukkan nama File Kepemilikan Tiket: ')
+File[5] = 'refund.csv'      # input('Masukkan nama File Refund Tiket: ')
+File[6] = 'kritiksaran.csv'    
+'''
+
+Filename = Filename
+
+    
+def login():
+    admin = False
+    un = input("Masukkan username: ")
+    pw = input("Masukkan password: ")
     login = False
-    for row in csv_reader:
-        if (row['Username']==un and row['Password']==pw):
-            user = row['Nama']
+    admin == False
+    for i in range (len(Filename[0])):
+        if Filename[0][i]['Username'] == un and Filename[0][i]["Password"] == pw:
+            user = Filename[0][i]['Nama']
             login = True
+            default_User = i
             break
+    if Filename[0][i]['Role'] == "Admin":
+        admin = True
     if (login == True):
         print("Selamat bermain, {}!".format(user))
+        return default_User
     else:
         print("Ups, password salah atau kamu tidak terdaftar dalam sistem kami. Silakan coba lagi!")
+
+user_id = login()
+
+def isAdmin(id):
+    adm = False
+    if Filename[0][id]['Role'] == 'Admin':
+        adm == True
+    return adm
+
+admin = isAdmin(user_id)
