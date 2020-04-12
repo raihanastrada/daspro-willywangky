87a5721a6217e958f8a784d74fc99abaf44f16db
sidarthaprastya <sidarthaprastya@gmail.com>
2020-04-12 10:37:56 +0700

    update

---
+++ index.py
@@ -0,0 +1,59 @@
+import os
+import csv
+
+def load():
+    fileuser =[]
+    filewahana =[]
+    filepembelian =[]
+    filepenggunaan=[]
+    filekepemilikan =[]
+    filerefund =[]
+    kritiksaran =[]
+    
+    file_user = input("Masukkan nama File User: ")
+    file_wahana = input("Masukkan nama File Wahana:")
+    file_pembelian = input("Masukkan nama File Pembelian Tiket: ")
+    file_penggunaan = input("Masukkan nama File Penggunaan Tiket: ")
+    file_kepemilikan = input("Masukkan nama File Kepemilikan Tiket: ")
+    file_refund = input("Masukkan nama File Refund Tiket: ")
+    file_kritiksaran = input("Masukkan nama File Kritik dan Saran: ")
+    with open(file_user , 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter =',')
+        for row in read:
+            fileuser.append(row)
+    with open(file_wahana, 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter=',')
+        for row in read:
+            filewahana.append(row)
+    with open(file_pembelian, 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter=',')
+        for row in read:
+            filewahana.append(row)
+    with open(file_pembelian, 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter=',')
+        for row in read:
+            filepembelian.append(row)
+    with open(file_penggunaan, 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter=',')
+        for row in read:
+            filepenggunaan.append(row)
+    with open(file_kepemilikan, 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter=',')
+        for row in read:
+            filekepemilikan.append(row)
+    with open(file_refund, 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter=',')
+        for row in read:
+            filerefund.append(row)
+    with open(file_kritiksaran, 'r') as csv_file:
+        read = csv.DictReader(csv_file, delimiter=',')
+        for row in read:
+            kritiksaran.append(row)
+    print("File perusahaan Willy Wangky’s Chocolate Factory telah di-load.")
+    
+    return fileuser, filewahana, filepembelian, filepenggunaan, filekepemilikan, filerefund, kritiksaran
+
+
+
+print("Selamat Datang di Willy Wangky")
+load()
--- tambah_wahana.py
+++ tambah_wahana.py
@@ -1,13 +1,16 @@
+import csv
+
 def tambah_wahana():
     with open('wahana.csv','r') as read:
-        csv_reader = csv.DictReader(read)
+        csv_reader = csv.DictReader(read) 
 
         with open('wahana.csv','a',newline='') as write:
-            csv_writer = csv.DictWriter(write)
+            fieldnames = ['ID_Wahana','Nama_Wahana','Harga_Tiket','Batasan_Umur','Batasan_Tinggi']
+            csv_writer = csv.DictWriter(write,fieldnames=fieldnames)
             print("Masukkan Informasi Wahana yang ditambahkan: ")
             id = input("Masukkan ID Wahana: ")
-            nama = input("Masukkan Nama Wahana")
-            harga = input("Masukkan Harga Tiket")
+            nama = input("Masukkan Nama Wahana: ")
+            harga = input("Masukkan Harga Tiket: ")
             umur = input("Batasan Umur: ")
             tinggi = input("Batasan Tinggi Badan: ")
 
@@ -21,3 +24,50 @@ def tambah_wahana():
             }) 
 
             print("Info wahana telah ditambahkan!")
+
+def replace():
+    with open('wahana.csv','r') as read:
+        csv_reader = csv.DictReader(read)
+        print("Ketik ID Wahana yang ingin anda ganti: ")
+        ID = input()
+        valid = False
+        while (not valid):
+            for row in csv_reader:
+                if (row['ID_Wahana']==ID):
+                    valid = True
+                    print("Berikut info wahana yang ingin anda ubah: ")
+                    print(row['ID_Wahana'])
+                    print(row['Nama_Wahana'])
+                    print(row['Harga_Tiket'])
+                    print(row['Batasan_Umur'])
+                    print(row['Batasan_Tinggi']+'\n')
+                    break
+            if (not valid):
+                print("Data tidak ada, silakan ulangi lagi")
+                ID = input()
+        with open('wahana.csv','w',newline='') as write:
+            fieldnames = ['ID_Wahana','Nama_Wahana','Harga_Tiket','Batasan_Umur','Batasan_Tinggi']
+            csv_writer = csv.DictWriter(write,fieldnames=fieldnames)
+            csv_writer.writeheader()
+            for row in csv_reader:
+                if (row['ID_Wahana']!=ID):
+                    csv_writer.writerow(row)
+        
+        with open('wahana.csv','a',newline='') as append:
+            replace = csv.DictWriter(append,fieldnames=fieldnames)
+            print("Silakan isi data baru tentang wahana ini: ")
+            id = input("Masukkan ID Wahana: ")
+            nama = input("Masukkan Nama Wahana: ")
+            harga = input("Masukkan Harga Tiket: ")
+            umur = input("Batasan Umur: ")
+            tinggi = input("Batasan Tinggi Badan: ")
+            replace.writerow({
+                'ID_Wahana' : id,
+                'Nama_Wahana' : nama,
+                'Harga_Tiket' : harga,
+                'Batasan_Umur' : umur,
+                'Batasan_Tinggi' : tinggi
+            })
+
+
+replace()
