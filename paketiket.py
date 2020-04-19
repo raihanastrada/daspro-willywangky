30a8a9dc66e11168aca054ed67194e94bdd208ec
tugusav <61377786+tugusav@users.noreply.github.com>
2020-04-20 00:53:17 +0700

    first attempt of paketiket

--- paketiket.py
+++ paketiket.py
@@ -1,5 +1,5 @@
 from load import Filename
-from login import user_id, admin
+from login import username, user_birth, user_height, user_saldo
 '''
 File[0] = 'user.csv'        # input('Masukkan nama File User: ')
 File[1] = 'wahana.csv'      # input('Masukkan nama File Daftar Wahana: ')
@@ -10,31 +10,33 @@ File[5] = 'refund.csv'      # input('Masukkan nama File Refund Tiket: ')
 File[6] = 'kritiksaran.csv'    
 '''
 
-Filename = Filename
-user_id = user_id
-admin = admin
-
-def pakai():
+def main():
+    arrTiket = Filename[4]
     arrPenggunaan = Filename[3]
     arrPembelian = Filename[2]
     arrUser = Filename[0]
     wahana = input("Masukkan ID wahana: ")
     tanggal = input("Masukkan tanggal hari ini: ")
-    jml_tiket = input("Masukkan tiket yang digunakan: ")
+    jml_tiket = int(input("Masukkan tiket yang digunakan: "))
     valid = False
     i = 0
-    while not(valid) and i < len(arrPembelian):
-        if arrPenggunaan[i]['Username'] == arrUser[user_id]['Username'] and arrPembelian[i]["ID_Wahana"] == wahana and arrPembelian[i]['Jumlah_Tiket'] <= jml_tiket:
-            valid = True
-    if valid:
+    while not(valid) and i < 1000 : # searching kepemilikan tiket dari user
+        if arrTiket[i][0] == username and arrTiket[i][1] == wahana: 
+            if int(arrTiket[i][2]) >= jml_tiket:
+                valid = True
+                arrTiket[i][2] = int(arrTiket[i][2]) - jml_tiket # mengurangi jumlah tiket pada array kepemilikan
+                break
+            else:
+                print("Maaf tiket anda kurang! Silakan membeli tiket lagi.")
+                break
+        else:
+            i += 1 # next element
+    if valid: # jika user memiliki tiket yang cukup
         print("Terima kasih telah bermain.")
-        arrPenggunaan.append({
-            'Username': arrUser[user_id]['Username'],
-            'Tanggal_Penggunaan': tanggal,
-            'ID_Wahana': wahana,
-            'Jumlah_Tiket': jml_tiket,
-        })
-    
+        for i in range(1000):
+            if arrPenggunaan[i] == None:
+                arrPenggunaan[i] = [username, tanggal, wahana, jml_tiket] # menambahkan data ke file penggunaan tiket
+                break
     else:
         print("Tiket Anda tidak valid dalam sistem kami")
 
