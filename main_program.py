8ede4fe2f275b5d9a9224ef1e9d5d507ce415131
tugusav <avanindra.tugus@outlook.com>
2020-04-26 16:02:24 +0700

    nambahin credit

--- main_program.py
+++ main_program.py
@@ -1,6 +1,15 @@
 import csv
 import os
 
+'''
+Ini adalah project Tugas Besar Daspro Kelompok 8 K05
+Anggota:
+1. 16519045 - Ida Bagus Raditya A.M.
+2. 16519225 - Raihan Astrada Fathurrahman
+3. 16519305 - Christopher Chandrasaputra
+4. 16519405 - Sidartha Prastya. P
+'''
+
 
 def clear():
     os.system('cls')
